86b08db8a6c51e5e93bd5afef6516e985c307580
Jonatass06 <jonatas_goncalves@estudante.sesisenai.org.br>
2024-04-17 14:56:53 -0300

    primeira entidade pronta

--- src/main/java/br/org/sesisenai/ava/entity/Usuario.java
+++ src/main/java/br/org/sesisenai/ava/entity/Usuario.java
@@ -6,15 +6,18 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.LocalDateTime;
+import java.util.Collection;
 import java.util.Set;
 
 @Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class Usuario implements ResponseConversorDTO<UsuarioResponseDTO> {
+public class Usuario implements ResponseConversorDTO<UsuarioResponseDTO>, UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -45,6 +48,42 @@ public class Usuario implements ResponseConversorDTO<UsuarioResponseDTO> {
     public Usuario(Long id) {
         this.id = id;
     }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.senha;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.nome;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
 }
 
 
